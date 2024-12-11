fd693d314bec165fe1be8681c4f32424db0420bb
Irina-Zhuk <irinamzh@gmail.com>
2024-12-10 23:37:17 -0500

    DatePiker fromdate was developed

--- pageObject/LambdaTest/Solutions/DatePicker.ts
+++ pageObject/LambdaTest/Solutions/DatePicker.ts
@@ -5,13 +5,52 @@ export class DatePicker {
   constructor(page:Page) {
     this.page = page
   }
+  //selectors
   header = "h1"
   url = 'https://www.lambdatest.com/selenium-playground/jquery-date-picker-demo'
-  async open() {
+  // From
+
+  fromInput = '#from'
+  prevMonth = "[title='Prev']"
+  dayOfTheYear =
+  dayOfTheMonth =
+  dayFromComponent =
+  public async open() {
     await this.page.goto(this.url)
   }
-  async verifyHeader(){
+  public async verifyHeader(){
      const header = this.page.locator (this.header)
      await expect (header).toContainText('Date Picker')
   }
+  randomYearNumber = _.random(1,50)
+  date = _.random(1,30)
+  public async dateFromToday(){
+    await this.page.locator(this.fromInput).click()
+    for (let i=0; i < randomNumber; i++)
+    await this.page.locator(this.prevMonth).click()
+
+    let year = await this.page.locator(this.dayOfTheYear).textContent()
+    let month = await this.page.locator(this.dayOfTheMonth)
+      .locator('')
+      .textContent()
+    let obj = {
+      Jan:"01",
+      Feb:"02",
+      Mar:"03",
+      Apr:"04",
+      May:"05",
+      Jun:"06",
+      Jul:"07",
+      Aug:"08",
+      Sep:"09",
+      Oct:"10",
+      Nov:"11",
+      Dec:"12"
+    }
+
+
+    await this.page.pause()
+  }
+
+
 }
--- tests/lambdaTest/datePicker.spec.ts
+++ tests/lambdaTest/datePicker.spec.ts
@@ -2,11 +2,12 @@ import {test as it} from "@playwright/test"
 import {DatePicker} from "../../pageObject/LambdaTest/Solutions/DatePicker";
 
 it.describe ('Date Picker PAGE', () => {
-  it ('should have a header',  async ({page}) => {
+  it ('set random ate for From input field',  async ({page}) => {
     const datePicker = new DatePicker(page)
     await datePicker.open()
     // page.goto ('https://www.lambdatest.com/selenium-playground/jquery-date-picker-demo')
     await datePicker.verifyHeader()
+    await datePicker.dateFromToday()
   })
 })
 
