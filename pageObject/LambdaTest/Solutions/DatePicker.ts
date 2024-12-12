79bfabe3b72b6f64cbffc88e67c1d4298ad0637e
Irina-Zhuk <irinamzh@gmail.com>
2024-12-11 19:03:53 -0500

    Addded test for To component

--- pageObject/LambdaTest/Solutions/DatePicker.ts
+++ pageObject/LambdaTest/Solutions/DatePicker.ts
@@ -16,6 +16,13 @@ export class DatePicker {
   dateOfTheYear = ".ui-datepicker-year"
   dateOfTheMonth = '[class="ui-datepicker-month"]'
   dayFromComponent = '#ui-datepicker-div'
+  // To
+  toInput = '#to'
+  prevMonthTo = "[title='Prev']"
+  nextMonthTo = "[title='Next']"
+  dateOfTheYearTo = ".ui-datepicker-year"
+  dateOfTheMonthTo = '[class="ui-datepicker-month"]'
+  dayToComponent = '#ui-datepicker-div'
 
   public async open() {
     await this.page.goto(this.url)
@@ -26,7 +33,10 @@ export class DatePicker {
   }
   // randomYearNumber = Math.floor(Math.random() * 1000)
   randomYearNumber = _.random(1,50)
-  day = _.random(1,30)
+
+  // day = _.random(1, 31).toString().padStart(2, '0');
+
+  day = _.random(1, 31).toString().padStart(2, '0');
   public async dateFromToday(){
     let obj = {
       Jan:"01",
@@ -65,5 +75,50 @@ export class DatePicker {
     expect(await this.page.locator(this.fromInput).inputValue()).toBe(
       `${obj[month]}/${this.day}/${year}`,
     ); //mm/dd/yyyy
+  }
+  public async dateToToday() {
+    let obj1 = {
+      Jan:"01",
+      Feb:"02",
+      Mar:"03",
+      Apr:"04",
+      May:"05",
+      Jun:"06",
+      Jul:"07",
+      Aug:"08",
+      Sep:"09",
+      Oct:"10",
+      Nov:"11",
+      Dec:"12"
+    }
+    await this.page.locator(this.toInput).click()
+    for (let i=0; i < this.randomYearNumber; i++) {
+      await this.page.locator(this.nextMonthTo).click()
+    }
+    console.log (this.randomYearNumber)
+
+    let year = await this.page.locator(this.dateOfTheYear).textContent()
+    let month = await this.page
+      .locator(this.dateOfTheMonth)
+      .locator('[selected="selected"]')
+      .textContent()
+    console.log(year, 'year')
+    console.log(month, 'month')
+    console.log(this.day, 'day')
+
+    await this.page
+      .locator(this.dayToComponent)
+      .getByRole('link', {name: this.day, exact: true})
+      .click()
+
+    expect(await this.page.locator(this.toInput).inputValue()).toBe(
+      `${obj1[month]}/${this.day}/${year}`,
+    ); //mm/dd/yyyy
+
+
+
+
+
+
   }
 }
--- tests/lambdaTest/datePicker.spec.ts
+++ tests/lambdaTest/datePicker.spec.ts
@@ -2,12 +2,22 @@ import {test as it} from "@playwright/test"
 import {DatePicker} from "../../pageObject/LambdaTest/Solutions/DatePicker";
 
 it.describe ('Date Picker PAGE', () => {
-  it ('set random ate for From input field',  async ({page}) => {
+
+  it ('set random date for From input field',  async ({page}) => {
     const datePicker = new DatePicker(page)
     await datePicker.open()
     // page.goto ('https://www.lambdatest.com/selenium-playground/jquery-date-picker-demo')
     await datePicker.verifyHeader()
     await datePicker.dateFromToday()
+
+  })
+  it ('set random date for To input field',  async ({page}) => {
+    const datePicker = new DatePicker(page)
+    await datePicker.open()
+
+    await datePicker.verifyHeader()
+    await datePicker.dateToToday()
+    // await page.pause ()
   })
 })
 
